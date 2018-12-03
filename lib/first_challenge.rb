e901e7ed436cc0f5ba74dfe526b246b529ad0661
Jonathan Harwood <jhharwood88@gmail.com>
2018-12-03 00:19:52 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,10 @@
 def first_challenge
+<<<<<<< HEAD
    epic_tragedy = {
    :montague => {} , 
    :capulet => {}
    }
+=======
+   epic_tragedy = {:montague}
+>>>>>>> 283185712f7cea04c4880c44b28f79520781fd25
 end
